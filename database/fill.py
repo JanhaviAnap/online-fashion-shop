bda2df538015fa5c577db2911728af14074adcf0
Janhavi Anap <63518181+JanhaviAnap@users.noreply.github.com>
2022-07-20 01:35:17 +0530

    updated fill.py

--- database/fill.py
+++ database/fill.py
@@ -9,6 +9,76 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+# create table user
+sql = """CREATE TABLE `onlineshop`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email_id` VARCHAR(450) NULL,
+  `password` VARCHAR(45) NULL,
+  `user_type` VARCHAR(45) NULL,
+  `user_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+"""
+mycursor.execute(sql)
+
+# create table product
+sql = """CREATE TABLE `onlineshop`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NULL,
+  `description` VARCHAR(5000) NULL,
+  `image_url` VARCHAR(600) NULL,
+  `unit_price` DECIMAL(13,2) NULL,
+  `units_in_stock` INT NULL,
+  `category_id` INT NULL,
+  PRIMARY KEY (`id`))
+"""
+mycursor.execute(sql)
+
+# create table product category
+sql = """CREATE TABLE `onlineshop`.`product_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(500) NULL,
+  `type` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`))
+"""
+mycursor.execute(sql)
+
+# create table cart item
+sql = """CREATE TABLE `onlineshop`.`cart_item` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_email` VARCHAR(450) NULL,
+  `cart_id` INT NULL,
+  `product_id` INT NULL,
+  `product_name` VARCHAR(200) NULL,
+  `product_image_url` VARCHAR(200) NULL,
+  `product_unit_price` INT NULL,
+  `quantity` INT NULL,
+  PRIMARY KEY (`id`))
+"""
+mycursor.execute(sql)
+
+# create table cart history
+sql = """CREATE TABLE `onlineshop`.`cart_history` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_email` VARCHAR(450) NULL,
+  `total_quantity` INT NULL,
+  `total_price` INT NULL,
+  `payment_status` VARCHAR(45) DEFAULT '"unpaid"',
+  `name` VARCHAR(200) NULL,
+  `email` VARCHAR(45) NULL,
+  `address` VARCHAR(200) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `pincode` INT NULL,
+  `last_modified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+"""
+mycursor.execute(sql)
+
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+  print(x)
+
 sql = "INSERT INTO product_category VALUES (%s,%s,%s)"
 file = open('product_category.csv')
 csvreader = csv.reader(file)
@@ -47,5 +117,6 @@ for row in rows:
     mycursor.execute(sql,val)
     print(row)
 
-mydb.commit()
 file.close()
+
+mydb.commit()
